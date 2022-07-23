92e17846c24c0fc96aaa4c02594584197db903bc
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-07-23 02:14:02 -0400

    starting blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,5 @@
 class BlogsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
   resources :comments
   resources :blogs
   resources :users
