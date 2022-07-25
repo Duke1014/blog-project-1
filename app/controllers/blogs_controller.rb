de8b652d8b3c569248f77faa3e5bccee2cbf94d5
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-07-25 14:12:32 -0400

    blog controller setup

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,38 @@
 class BlogsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
   def index
     @blogs = Blog.all
   end
 
-  def show
+  # def show
 
+  # end
+
+  def create
+    user = User.find_by(id: session[:user_id])
+    blog = user.blogs.create!(blog_params)
+    render json: blog, status: :created
+  end
+
+  # def update
+    
+  # end
+
+  def destroy
+    blog = Blog.find_by(id: params[:id])
+    blog.destroy
+    head :no_content
+  end
+
+  private
+
+  def render_unprocessable_entity(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def blog_params
+      params.permit(:content)
   end
 
 end
