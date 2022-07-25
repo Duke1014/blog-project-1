3f6aa9c7efc3f61c2e45b783f908eee5a0e814c0
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-07-25 14:00:02 -0400

    comment controller create and destroy added, get modules needed later

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,38 @@
 class CommentsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    # def index
+
+    # end
+
+    # def show
+
+    # end
+
+    def create
+        user = User.find_by(id: session[:user_id])
+        comment = user.comments.create!(comment_params)
+        render json: comment, status: :created
+    end
+
+    # def update
+
+    # end
+
+    def destroy 
+        comment = Comment.find_by(id: params[:id])
+        comment.destroy
+        head :no_content
+    end
+
+    private
+
+    def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def comment_params
+        params.permit(:content)
+    end
+
 end
