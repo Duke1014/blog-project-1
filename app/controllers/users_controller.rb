eacb753477d9754f693c77a2dad2f8be53efa3f6
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-07-25 13:41:12 -0400

    user controller setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,28 @@
 class UsersController < ApplicationController
-
-    def index
-    end
-
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    
     def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else
+            render json: { error: "Not Authorized" }, status: :unauthorized
+        end
     end
 
     def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
     end
 
-    def update
+    private
+
+    def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 
-    def destroy
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
     end
-    
 end
