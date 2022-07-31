75b7507516258ac3fcdd4ed7059076db9bdc8ed1
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-07-30 22:12:39 -0400

    organizing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
   
+  # ROOT
   root 'blogs#index'
 
+  # USERS
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+  
+  # SESSIONS
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  # BLOGS
   get '/blogs', to: 'blogs#index'
   get '/blogs/:id', to: 'blogs#show'
 
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  
+  # COMMENTS
+
+  # RESOURCES
   resources :comments
   resources :blogs
   resources :users
